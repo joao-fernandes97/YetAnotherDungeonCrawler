0fbc6205d710f35df61706f3f7756a7f2df1b3d1
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-18 13:59:04 +0100

    Created View class

---
+++ YetAnotherDungeonCrawler/View.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetAnotherDungeonCrawler
+{
+    public class View
+    {
+        public void ReadInput()
+        {
+
+        }
+        public void PrintOutput()
+        {
+
+        }
+        public void ReadDungeonMap()
+        {
+
+        }
+        public void ReadRoomConfig()
+        {
+            
+        }
+    }
+}
