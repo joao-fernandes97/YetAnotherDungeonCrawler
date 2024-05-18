9b88729e16d241e7a25f1b0ae01b998ce739ad26
Inês Gomes <inesgomes.pessoal05@gmail.com>
2024-05-18 14:38:44 +0100

    Created Character class

---
+++ YetAnotherDungeonCrawler/Character.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YetAnotherDungeonCrawler
+{
+    public class Character
+    {
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
+    }
+}
